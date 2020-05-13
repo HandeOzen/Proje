cd75176559d0d38717aa708a0e350319ba0f372e
HandeOzen <62430864+HandeOzen@users.noreply.github.com>
2020-05-13 17:31:03 +0300

    ProjeUpload
    Proje

---
+++ B181200048_Hande Özen/Ndp_Odev3/Sepet.cs
@@ -0,0 +1,39 @@
+﻿/****************************************************************************
+**                        SAKARYA ÜNİVERSİTESİ
+**            BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**              BİLİŞİM SİSTEMLERİ MÜHENDİSLİĞİ BÖLÜMÜ
+**                NESNEYE DAYALI PROGRAMLAMA DERSİ
+**                    2019-2020 BAHAR DÖNEMİ
+**
+**           PROJE NUMARASI.........: 01
+**           ÖĞRENCİ ADI............:HANDE ÖZEN
+**           ÖĞRENCİ NUMARASI.......:B181200048
+**           DERSİN ALINDIĞI GRUP...: A
+****************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ndp_Odev3
+{
+    class Sepet
+    {
+        public double kdvliToplamFiyat = 0;
+        public string ad="";
+        public int adet=0;
+        
+        public double kdvliToplamFiyatHesapla(CepTelefonu ceptelefonu,LedTv ledtv,Buzdolabi buzdolabi,Laptop laptop)
+        {
+            kdvliToplamFiyat = ceptelefonu.kdvUygula() + laptop.kdvUygula() + ledtv.kdvUygula() + buzdolabi.kdvUygula();
+            return kdvliToplamFiyat;
+        }
+        
+        public void sepeteUrunEkle(Urun urun)
+        {
+            ad = urun.ad;
+            adet = urun.secilenAdet;       
+        }
+    }
+}
---
+++ B181200048_Hande Özen/Ndp_Odev3/Urun.cs
@@ -0,0 +1,37 @@
+﻿/****************************************************************************
+**                        SAKARYA ÜNİVERSİTESİ
+**            BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**              BİLİŞİM SİSTEMLERİ MÜHENDİSLİĞİ BÖLÜMÜ
+**                NESNEYE DAYALI PROGRAMLAMA DERSİ
+**                    2019-2020 BAHAR DÖNEMİ
+**
+**           PROJE NUMARASI.........: 01
+**           ÖĞRENCİ ADI............:HANDE ÖZEN
+**           ÖĞRENCİ NUMARASI.......:B181200048
+**           DERSİN ALINDIĞI GRUP...: A
+****************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ndp_Odev3
+{
+    class Urun
+    {
+       Random rand = new Random();
+       public string ad;
+       public string marka;
+       public string model;
+       public string ozellik;
+       public int stokAdedi;
+       public double hamFiyat;
+       public int secilenAdet;
+
+        public Urun()
+        {
+            stokAdedi = rand.Next(1, 100);
+        }  
+    }
+}
